f5aff3db5d42d5b79f4739d87e7fba225264937a
momantai <megaoz100@hotmail.com>
2017-04-04 19:08:39 -0500

    Productos y Control.

--- NetBeansProjects/PuntoVenta/src/conexion/Control.java
+++ NetBeansProjects/PuntoVenta/src/conexion/Control.java
@@ -1,11 +1,37 @@
 
 package conexion;
-import entidades.Login;
-import conexion.Conexion;
+import com.mysql.jdbc.Connection;
+import entidades.Productos;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 /**
  *
  * @author momantai
  */
-public class Control {
-    
+public class Control  extends Conexion{
+
+    private PreparedStatement stmt;
+    public void registrarProductos(Productos producto){
+        int a = producto.getCodigo();
+        String b = producto.getDescripcion();
+        int c = producto.getClasificacion();
+        float d = producto.getPrecio();
+        int e = producto.getActivo();
+        
+        try {
+            Connection con;
+            con = conectar();
+            stmt = con.prepareStatement("INSER INTO productos VALUES(?, ?, ?, ?, ?)");
+            stmt.setInt(1, a);
+            stmt.setString(2, b);
+            stmt.setInt(3, c);
+            stmt.setFloat(4, d);
+            stmt.setInt(5, e);
+            
+            stmt.executeUpdate();
+            
+        } catch(SQLException eE) {
+            System.out.println("" + eE);
+        }
+    }
 }
