3a4c764617fdb57d7c717789fdea76b7dc25af48
Unknown <awelo.romero.mariscal@gmail.com>
2017-05-16 19:14:47 -0500

    Metodo de modificar productos reparado

--- NetBeansProjects/PuntoVenta/src/conexion/Control.java
+++ NetBeansProjects/PuntoVenta/src/conexion/Control.java
@@ -45,7 +45,7 @@ public class Control  extends Conexion{
     }
     
     public byte modificarProductos(Productos producto){
-        String sql = "UPDATE productos SET descripcion=?, clasificacion=?, precio=?, activo=?, imagen=? WHERE idCodigo="+producto.getCodigo();
+        String sql = "UPDATE productos SET descripcion=?, clasificacion=?, precio=?, activo=? WHERE idCodigo="+producto.getCodigo();
         byte exito=0;
         try{
             stmt = conectar().prepareStatement(sql);
@@ -53,7 +53,6 @@ public class Control  extends Conexion{
             stmt.setInt(2, producto.getClasificacion());
             stmt.setFloat(3, producto.getPrecio());
             stmt.setInt(4, producto.getActivo());
-            stmt.setString(5, producto.getImagen());
             
             stmt.executeQuery();
             exito=1;
@@ -116,12 +115,84 @@ public class Control  extends Conexion{
     public ResultSet empleadoFilt(String dato){
         ResultSet obtener= null;
         try{
-            String sql="SELECT nombreE, apellidosE, edad, telefono, users.user, password, permisos, domicilio.calle, numero, numeroInt, colonia FROM empleado INNER JOIN users ON empleado.user=users.idUser INNER JOIN domicilio ON empleado.domicilio=empleado.domicilioE WHERE idEmpleado="+dato;
+            String sql="SELECT nombreE, apellidosE, edad, telefono, users.idUser, users.user, password, permisos, domicilio.idDomicilio, calle, numero, numeroInt, colonia FROM empleado INNER JOIN users ON empleado.user=users.idUser INNER JOIN domicilio ON empleado.domicilioE=domicilio.idDomicilio WHERE idEmpleado="+dato;
             Statement stat=conectar().createStatement();
             obtener=stat.executeQuery(sql);
         }catch(SQLException e){
             System.out.println("Error al cargar "+e);
         }
         return obtener;
+    }
+    
+    public byte modificarEmpleado(String idDom, Domicilio domicilio, String idUsu, Usuario usuario, String idEmp, Empleado empleado){
+        byte exito=0;
+        try{
+            System.out.println("Desde aqui");
+            String sql="UPDATE users SET user=?, password=?, permisos=? WHERE idUser="+idUsu;
+            stmt=conectar().prepareStatement(sql);
+            stmt.setString(1, usuario.getUser());
+            stmt.setString(2, usuario.getPassword());
+            stmt.setInt(3, usuario.getPermisos());
+            if(stmt.executeUpdate()==1){
+                System.out.println("usuario bien");
+                sql="UPDATE domicilio SET calle=?, numero=?, numeroInt=?, colonia=? WHERE idDomicilio="+idDom;
+                stmt=conectar().prepareStatement(sql);
+                stmt.setString(1, domicilio.getCalle());
+                stmt.setString(2, domicilio.getNumExt());
+                stmt.setString(3, domicilio.getNumInt());
+                stmt.setString(4, domicilio.getColonia());
+                if(stmt.executeUpdate()==1){
+                    System.out.println("domicilio bien");
+                    sql="UPDATE empleado SET nombreE=?, apellidosE=?, edad=?, puesto=?, telefono=? WHERE idEmpleado="+idEmp;
+                    stmt=conectar().prepareStatement(sql);
+                    stmt.setString(1, empleado.getNombre());
+                    stmt.setString(2, empleado.getApellidos());
+                    stmt.setInt(3,empleado.getEdad());
+                    stmt.setString(4, empleado.getPuesto());
+                    stmt.setString(5, empleado.getTelefono());
+                    if(stmt.executeUpdate()==1){
+                        exito=1;
+                        System.out.println("empleado bien");
+                    }
+                }
+            }
+        } catch(SQLException e){
+            System.out.println("Error en la conexion");
+        }
+        return exito;
+    }
+    
+    public byte borrarEmpleado(String dato){
+        byte exito=0;
+        try{
+            String sql="SELECT users.idUSer, domicilio.idDomicilio FROM empleado INNER JOIN users ON empleado.user=users.idUser INNER JOIN domicilio ON empleado.domicilioE=domicilio.idDomicilio WHERE idEmpleado="+dato;
+            Statement stat= conectar().createStatement();
+            ResultSet obtener=stat.executeQuery(sql);
+            String[] ids = new String[2];
+            while(obtener.next()){
+                ids[0]=obtener.getString(1);
+                ids[1]=obtener.getString(2);
+            }
+            
+            sql="DELETE FROM users WHERE idUser="+ids[0];
+            stmt= conectar().prepareStatement(sql);
+            if(stmt.executeUpdate()==1){
+                System.out.println("user bien");
+                sql="DELETE FROM domicilio WHERE idDomicilio="+ids[1];
+                stmt=conectar().prepareStatement(sql);
+                if(stmt.executeUpdate()==1){
+                    System.out.println("domicilio bien");
+                    sql="DELETE FROM empleado WHERE idEmpleado="+dato;
+                    stmt= conectar().prepareStatement(sql);
+                    if(stmt.executeUpdate()==1){
+                        exito=1;
+                    }
+                }
+            }
+            
+        } catch(SQLException e){
+            System.out.println("Error en la conexion "+e);
+        }
+        return exito;
     }
 }
